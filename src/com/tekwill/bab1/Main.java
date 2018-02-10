053ff1994c24c9e40d7327a10e4270ce407c0bf7
Mihail Pecari <mihail.pecari@extenda.com>
2018-02-10 13:23:43 +0200

    initial commit

---
+++ src/com/tekwill/bab1/Main.java
@@ -0,0 +1,10 @@
+package com.tekwill.bab1;
+
+/**
+ * Created by mpecari on 2/10/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test my lab");
+    }
+}
